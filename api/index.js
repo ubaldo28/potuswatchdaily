a7610cd015675d4bc417e28fd608917c2b2996a0
Ubi <potuswatchdaily@gmail.com>
2026-05-10 23:33:05 +0100

    Cleanup: remove stale files and SSR-blocking public/index.html

--- api/index.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-module.exports = async (req, res) => {
-  const html = fs.readFileSync(path.join(__dirname, '../public/index.html'), 'utf8');
-  res.setHeader('Content-Type', 'text/html');
-  res.send(html);
-};
