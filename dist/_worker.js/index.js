4ab5de42dd295cfe293bab8fb0fa2a2f0fd0d1d9
Ubi <potuswatchdaily@gmail.com>
2026-05-21 14:28:24 +0100

    Fix wrangler assets config

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CqRP-xIa.mjs';
-import { manifest } from './manifest_qztFgijK.mjs';
+import { manifest } from './manifest_BpFuSQqS.mjs';
 
 const serverIslandMap = new Map();;
 
