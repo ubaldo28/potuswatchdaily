fc89d0f956398fd5126faad144700dc4dc0638dd
Ubi <potuswatchdaily@gmail.com>
2026-05-25 22:39:33 +0100

    Update both Adsterra script URLs

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CqRP-xIa.mjs';
-import { manifest } from './manifest_Cqx70CiZ.mjs';
+import { manifest } from './manifest_D22sDNar.mjs';
 
 const serverIslandMap = new Map();;
 
