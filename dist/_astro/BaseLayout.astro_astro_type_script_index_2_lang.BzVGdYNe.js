592f469cb202d67f08d5ad29cab5143b8c50ee39
Ubi <potuswatchdaily@gmail.com>
2026-05-25 22:36:55 +0100

    Update Adsterra script URL

--- dist/_astro/BaseLayout.astro_astro_type_script_index_2_lang.BzVGdYNe.js
+++
@@ -1,1 +0,0 @@
-import"https://actingprofessionshiny.com/37/c5/f9/37c5f97dcf85aef347afb415f071ce90.js";
--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CqRP-xIa.mjs';
-import { manifest } from './manifest_BNpWruVP.mjs';
+import { manifest } from './manifest_Cqx70CiZ.mjs';
 
 const serverIslandMap = new Map();;
 
